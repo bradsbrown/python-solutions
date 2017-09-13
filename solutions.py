4d14be62752477a8dbfb1105730ae9239a370929
CHBaker <charlesprobaker@mail.com>
2017-09-13 08:17:05 -0600

    FEATURE/BUG: MST PYTHON
    BUG: heavy edge is being added to vertex B at end of function

--- solutions.py
+++ solutions.py
@@ -55,5 +55,84 @@
 # print question2(3,3,'hey'), "\n"
 # # Should return False
 
+def question3(adjDict):
+	edgeList = []
+	seen = []
+
+	for vertex in adjDict.iterkeys():
+		print 'VERTEX ', vertex
+
+		# set first edge as min and compare
+		minEdge = adjDict[vertex][0]
+		print 'Min Edge ', minEdge
+
+		for edge in adjDict[vertex]:
+			print 'EDGE ', edge
+
+			if edge[0] in seen:
+				print 'backtrack ', edge[0]
+				add = list(edge)
+				add.pop(0)
+				add.insert(0, vertex)
+				add = tuple(add)
+
+				pin = seen.index(edge[0])
+				pin = seen[pin]
+				print 'pin ', pin
+				print 'ADDDDD ', add
+				print 'is it in? ', adjDict[pin]
+
+				if add in adjDict[pin]:
+					print 'add is already in, skipping... may be adding to edge list'
+					if edge not in edgeList:
+						print 'not in edgelist, adding...'
+						edgeList.append(edge)
+
+				else:
+					print 'AFTER ADD VERTEX ', adjDict[pin]
+					print add, ' is not in the list!'
+					adjDict[pin].append(add)
+
+			if edge[1] < minEdge[1] and edge[1] != minEdge[1]:
+				print 'adding min edge>'
+				minEdge = edge
+
+		if minEdge not in edgeList:
+			edgeList.append(minEdge)
+
+		print "edgeList = ", edgeList
+
+		adjDict[vertex] = edgeList
+
+		print "Vertex List = ", adjDict[vertex]
+
+		edgeList = []
+		minEdge = ('', 0)
+		seen.append(vertex)
+
+
+	print "Dict Done = "
+	return adjDict
+
+
+a = {
+	'A': [('B', 3), ('D', 4)],
+	'B': [('A', 3), ('E', 4), ('F', 6)],
+	'C': [('E', 5)],
+	'D': [('A', 4)],
+	'E': [('B', 4), ('C', 5), ('F', 5)],
+	'F': [('B', 6), ('E', 5)]
+}
+
+print question3(a)
+# Should return
+# { 
+#   'A': [('B', 3), ('D', 4)],
+#	'B': [('A', 3), ('E', 4)],
+#	'C': [('E', 5)],
+#	'D': [('A', 4)],
+#	'E': [('B', 4), ('C', 5), ('F', 5)],
+#	'F': [('E', 5)]
+# }
 
 
